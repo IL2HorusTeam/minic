d3e6ef700803612d78483e2525f34247d1554fe6
Alexander Oblovatniy <oblovatniy@gmail.com>
2014-05-04 14:43:00 +0300

    Build missions frame.

--- minic/ui.py
+++ minic/ui.py
@@ -86,18 +86,20 @@ class MainWindow(gtk.Window):
         super(MainWindow, self).__init__()
 
         self.set_title(self.title)
-        self.set_size_request(250, 150)
+        self.set_size_request(350, 200)
         self.set_position(gtk.WIN_POS_CENTER)
         self.set_icon_from_file(image_path(self.icon_name))
         self.build_tray_icon()
 
         menu_bar = self.build_menu_bar()
         connection_frame = self.build_connection_frame()
+        missions_frame = self.build_missions_frame()
 
         # Build layout ---------------------------------------------------------
         vbox = gtk.VBox(False, 3)
         vbox.pack_start(menu_bar, expand=False, fill=False, padding=0)
         vbox.pack_start(connection_frame, expand=False, fill=True, padding=0)
+        vbox.pack_start(missions_frame, expand=False, fill=True, padding=0)
         self.add(vbox)
 
         root_service.set_callbacks(self.on_connection_done,
@@ -105,89 +107,6 @@ class MainWindow(gtk.Window):
                                    self.on_connection_lost)
         self.connect('delete-event', lambda w, e: w.hide() or True)
         self.show_all()
-
-    def build_menu_bar(self):
-        m_bar = gtk.MenuBar()
-
-        # Settings -------------------------------------------------------------
-        m_settings = gtk.MenuItem(_("Settings"))
-        m_settings.connect('button-press-event', self.on_menu_settings_click)
-        m_bar.append(m_settings)
-
-        # Quit -----------------------------------------------------------------
-        m_quit = gtk.MenuItem(_("Quit"))
-        m_quit.connect('activate', self.on_quit)
-        m_bar.append(m_quit)
-
-        return m_bar
-
-    def on_menu_settings_click(self, widget, event):
-        # Intercept only left mouse button
-        if event.button != 1:
-            return False
-        SettingsWindow().show()
-
-    def build_connection_frame(self):
-        # Stack ----------------------------------------------------------------
-        stack = gtk.Notebook()
-        self.connection_stack = stack
-        stack.set_show_tabs(False)
-        stack.set_show_border(False)
-
-        # Build pages ----------------------------------------------------------
-        def to_page(label, button):
-            hbox = gtk.HBox(False, 2)
-            hbox.pack_start(label, expand=False, fill=False, padding=0)
-            hbox.pack_end(button, expand=False, fill=False, padding=0)
-            stack.append_page(hbox)
-
-        # Disconnected page
-        label = gtk.Label(_("Not established"))
-        label.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse('#B00'))
-        button = gtk.Button(_("Connect"), gtk.STOCK_CONNECT)
-        button.connect('clicked', self.on_connect_clicked)
-        to_page(label, button)
-
-        # Connected page
-        label = gtk.Label(_("Established"))
-        label.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse('#080'))
-        button = gtk.Button(_("Disconnect"), gtk.STOCK_DISCONNECT)
-        button.connect('clicked', self.on_disconnect_clicked)
-        to_page(label, button)
-
-        # Connecting page
-        label = gtk.Label(_("Connecting") + '...')
-        label.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse('#555'))
-        button = gtk.Button(_("Stop"), gtk.STOCK_STOP)
-        button.connect('clicked', self.on_connect_stop_clicked)
-        to_page(label, button)
-
-        # Build frame ----------------------------------------------------------
-        alignment = gtk.Alignment(0.5, 0.5, 1.0, 1.0)
-        alignment.set_padding(3, 3, 3, 3)
-        alignment.add(stack)
-
-        frame = gtk.Frame(label=_("Server connection"))
-        frame.add(alignment)
-
-        return frame
-
-    def on_connect_clicked(self, widget):
-        try:
-            root_service.startService()
-        except Exception as e:
-            show_error(e)
-        else:
-            self.connection_stack.set_current_page(
-                MainWindow.CONNECTION_TABS.CONNECTING)
-
-    def on_connect_stop_clicked(self, widget):
-        self.stop_root()
-        self.connection_stack.set_current_page(
-            MainWindow.CONNECTION_TABS.DISCONNECTED)
-
-    def on_disconnect_clicked(self, widget):
-        self.stop_root()
 
     def build_tray_icon(self):
         icon = gtk.status_icon_new_from_file(image_path(self.icon_name))
@@ -236,14 +155,187 @@ class MainWindow(gtk.Window):
         menu.show_all()
         menu.popup(None, None, None, event_button, event_time)
 
+    def build_menu_bar(self):
+        m_bar = gtk.MenuBar()
+
+        # Settings -------------------------------------------------------------
+        m_settings = gtk.MenuItem(_("Settings"))
+        m_settings.connect('button-press-event', self.on_menu_settings_click)
+        m_bar.append(m_settings)
+
+        # Quit -----------------------------------------------------------------
+        m_quit = gtk.MenuItem(_("Quit"))
+        m_quit.connect('activate', self.on_quit)
+        m_bar.append(m_quit)
+
+        return m_bar
+
+    def on_menu_settings_click(self, widget, event):
+        # Intercept only left mouse button
+        if event.button != 1:
+            return False
+        SettingsWindow().show()
+
+    def on_quit(self, *args):
+        return self.stop_root().addBoth(lambda unused: gtk.main_quit())
+
+    def build_connection_frame(self):
+        # Stack ----------------------------------------------------------------
+        stack = gtk.Notebook()
+        self.connection_stack = stack
+        stack.set_show_tabs(False)
+        stack.set_show_border(False)
+
+        # Build pages ----------------------------------------------------------
+        def to_page(label, button):
+            hbox = gtk.HBox(False, 2)
+            hbox.pack_start(label, expand=False, fill=False, padding=0)
+            hbox.pack_end(button, expand=False, fill=False, padding=0)
+            stack.append_page(hbox)
+
+        # Disconnected page
+        label = gtk.Label(_("Not established"))
+        label.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse('#B00'))
+        button = gtk.Button(_("Connect"), gtk.STOCK_CONNECT)
+        button.set_tooltip_text(_("Connect to server"))
+        button.connect('clicked', self.on_connect_clicked)
+        to_page(label, button)
+
+        # Connected page
+        label = gtk.Label(_("Established"))
+        label.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse('#080'))
+        button = gtk.Button(_("Disconnect"), gtk.STOCK_DISCONNECT)
+        button.set_tooltip_text(_("Disconnect for server"))
+        button.connect('clicked', self.on_disconnect_clicked)
+        to_page(label, button)
+
+        # Connecting page
+        label = gtk.Label(_("Connecting") + '...')
+        label.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse('#555'))
+        button = gtk.Button(_("Stop"), gtk.STOCK_STOP)
+        button.set_tooltip_text(_("Stop connection attempt"))
+        button.connect('clicked', self.on_connect_stop_clicked)
+        to_page(label, button)
+
+        # Build frame ----------------------------------------------------------
+        alignment = gtk.Alignment(0.5, 0.5, 1.0, 1.0)
+        alignment.set_padding(3, 3, 3, 3)
+        alignment.add(stack)
+
+        frame = gtk.Frame(label=_("Server connection"))
+        frame.add(alignment)
+
+        return frame
+
+    def on_connect_clicked(self, widget):
+        try:
+            root_service.startService()
+        except Exception as e:
+            show_error(e)
+        else:
+            self.connection_stack.set_current_page(
+                MainWindow.CONNECTION_TABS.CONNECTING)
+
+    def on_connect_stop_clicked(self, widget):
+        self.stop_root()
+        self.connection_stack.set_current_page(
+            MainWindow.CONNECTION_TABS.DISCONNECTED)
+
+    def on_disconnect_clicked(self, widget):
+        self.stop_root()
+
     def stop_root(self):
         def errback(reason):
             LOG.error("Failed to stop root service: {0}".format(
                       unicode(reason.value)))
         return root_service.stopService().addErrback(errback)
 
-    def on_quit(self, *args):
-        return self.stop_root().addBoth(lambda unused: gtk.main_quit())
+    def build_missions_frame(self):
+        table = gtk.Table(rows=4, columns=2, homogeneous=False)
+        table.set_col_spacings(10)
+
+        def to_label(name):
+            label = gtk.Label(name)
+            label.set_alignment(xalign=0, yalign=0.5)
+            return label
+
+        # Name row -------------------------------------------------------------
+        label = to_label(_("Current"))
+        table.attach(label, 0, 1, 0, 1, gtk.FILL)
+
+        mission_selector = gtk.combo_box_new_text()
+        table.attach(mission_selector, 1, 2, 0, 1)
+
+        # State row ------------------------------------------------------------
+        label = to_label(_("State"))
+        table.attach(label, 0, 1, 1, 2, gtk.FILL)
+
+        label = to_label("MISSION STATE")
+        table.attach(label, 1, 2, 1, 2)
+
+        # Time left row --------------------------------------------------------
+        label = to_label(_("Time left"))
+        table.attach(label, 0, 1, 2, 3, gtk.FILL)
+
+        label = to_label("TIME LEFT")
+        table.attach(label, 1, 2, 2, 3)
+
+        # Controls row ---------------------------------------------------------
+        def to_button(stock):
+            image = gtk.Image()
+            image.set_from_stock(stock, gtk.ICON_SIZE_BUTTON)
+            button = gtk.Button()
+            button.add(image)
+            return button
+
+        hbox = gtk.HBox(False, 8)
+        alignment = gtk.Alignment(0.5, 0.5, 1.0, 1.0)
+        alignment.set_padding(5, 0, 0, 0)
+        alignment.add(hbox)
+
+        table.attach(alignment, 0, 2, 3, 4)
+
+        button = to_button(gtk.STOCK_GOTO_FIRST)
+        button.set_tooltip_text(_("First mission"))
+        hbox.pack_start(button, False, False, 0)
+
+        button = to_button(gtk.STOCK_MEDIA_PREVIOUS)
+        button.set_tooltip_text(_("Previous mission"))
+        hbox.pack_start(button, False, False, 0)
+
+        button = to_button(gtk.STOCK_MEDIA_STOP)
+        button.set_tooltip_text(_("Stop mission"))
+        hbox.pack_start(button, False, False, 0)
+
+        button = to_button(gtk.STOCK_MEDIA_PLAY)
+        button.set_tooltip_text(_("Run mission"))
+        hbox.pack_start(button, False, False, 0)
+
+        button = to_button(gtk.STOCK_REFRESH)
+        button.set_tooltip_text(_("Restart mission"))
+        hbox.pack_start(button, False, False, 0)
+
+        button = to_button(gtk.STOCK_MEDIA_NEXT)
+        button.set_tooltip_text(_("Next mission"))
+        hbox.pack_start(button, False, False, 0)
+
+        button = to_button(gtk.STOCK_GOTO_LAST)
+        button.set_tooltip_text(_("Last mission"))
+        hbox.pack_start(button, False, False, 0)
+
+        button = gtk.Button(_("Edit"), gtk.STOCK_EDIT)
+        button.set_tooltip_text(_("Edit missions"))
+        hbox.pack_end(button, False, False, 0)
+
+        # Build frame ----------------------------------------------------------
+        alignment = gtk.Alignment(0.5, 0.5, 1.0, 1.0)
+        alignment.set_padding(3, 3, 3, 3)
+        alignment.add(table)
+
+        frame = gtk.Frame(label=_("Mission"))
+        frame.add(alignment)
+
+        return frame
 
     def on_connection_done(self, *args):
         self.connection_stack.set_current_page(
